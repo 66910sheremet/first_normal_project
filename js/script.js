7594c4566e591ec349bdc0d6152fcd4896d597c3
Eugene <66910@mail.ru>
2021-10-16 12:04:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+//
+//a = 15
+//console.log(a);
+//
+//
+//let number = 5;
+//const leftBoarderWidth = 1;
+//
+//number = 10;
+//console.log(number);
+//
+//const obj = {
+//    a: 50
+//}
+//
+//obj.a = 10;
+//console.log(obj)
+//
+//let number1 = 4.6;
+//
+//console.log('sting' * 9)
+//
+//const persone = "Alex";
+//
+//const bool = true;
+//
+//console.log(something);
+//
+//let und;
+//
+//console.log(und);
+//
+////const obj1 ={
+//    name: "John",
+//    age: 25,
+//    isMarried: false
+//};
+
+//console.log(obj1.name)
+
+//console.log(obj1['name'])
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+
+//console.log(typeof(answer));
+
+//const answers = [];
+//
+//answers[0] = prompt("Как ваше имя?"," ")
+//answers[1] = prompt("Как вашa фамилия?"," ")
+//answers[2] = prompt("Сколько вам лет?"," ")
+//
+////document.write(answers);
+//
+//console.log(typeof(answers));
+
+//const category = 'toys';
+//
+//console.log(`https://someurl.com/${category}`);
+//
+//const user = 'Ivan';
+//
+//alert(`Привет,${user}`);
+
+let incr = 10;
+    decr = 10;
+
+//incr++;
+//decr--;
+
+//++incr;
+//--decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
